376d1669c63d6f3b169330f0005e142445abe4c0
Dudek <126721457+JakubDudeq@users.noreply.github.com>
2024-10-04 22:52:46 +0200

    Add files via upload

---
+++ dudqCOUT.h
@@ -0,0 +1,54 @@
+#ifndef BETTERCOUT_H
+#define BETTERCOUT_H
+
+using namespace std;
+
+template <typename T>
+void out(const T& arg) {
+    cout << arg << endl;
+}
+
+template <typename T, typename... Args>
+void out(const T& first, const Args&... args) {
+    cout << first;
+    out(args...);
+}
+
+template <typename T>
+void spout(const T& arg) {
+    cout << arg << " " << endl;
+}
+
+template <typename T, typename... Args>
+void spout(const T& first, const Args&... args) {
+    cout << first << " ";
+    spout(args...);
+}
+
+template <typename T>
+void enout(const T& arg) {
+    cout << arg << endl;
+}
+
+template <typename T, typename... Args>
+void enout(const T& first, const Args&... args) {
+    cout << first << endl;
+    enout(args...);
+}
+#endif // BETTERCOUT_H
+
+/*
+	/// Dokumentacja
+	
+    out(a, a, a...);
+    Wypisuje wszystkie podane wartoœci ko³o siebiê w konsoli i koñczy siê endl-inem
+    a (any) - wartoœæ do wypisania, iloœæ podanych argumentów jest dowolna
+
+    spout(a, a, a..);
+    Wypisuje wszystkie podane wartoœci ko³o siebiê oddzielaj¹c je spacj¹ w konsoli i koñczy siê endl-inem
+    a (any) - wartoœæ do wypisania, iloœæ podanych argumentów jest dowolna
+
+    enout(a, a, a..);
+    Wypisuje wszystkie podane wartoœci ko³o siebiê oddzielaj¹c je enterem i koñczy siê endl-inem
+    a (any) - wartoœæ do wypisania, iloœæ podanych argumentów jest dowolna
+*/
